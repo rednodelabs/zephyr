1a306a148f589960c47b28645ba20f83c0544ea6
Javier Garcia <javier@rednodelabs.com>
2022-02-05 21:52:15 +0100

    Add SPI blocking mode in order to improve access speed

--- drivers/spi/spi_context.h
+++ drivers/spi/spi_context.h
@@ -29,7 +29,9 @@ struct spi_context {
 	const struct spi_config *owner;
 
 	struct k_sem lock;
+#ifndef CONFIG_SPI_BLOCKING
 	struct k_sem sync;
+#endif /* CONFIG_SPI_BLOCKING */
 	int sync_status;
 
 #ifdef CONFIG_SPI_ASYNC
@@ -46,6 +48,10 @@ struct spi_context {
 	uint8_t *rx_buf;
 	size_t rx_len;
 
+#ifdef CONFIG_SPI_BLOCKING
+	volatile bool spi_complete;
+#endif /* CONFIG_SPI_BLOCKING */
+
 #ifdef CONFIG_SPI_SLAVE
 	int recv_frames;
 #endif /* CONFIG_SPI_SLAVE */
@@ -54,8 +60,10 @@ struct spi_context {
 #define SPI_CONTEXT_INIT_LOCK(_data, _ctx_name)				\
 	._ctx_name.lock = Z_SEM_INITIALIZER(_data._ctx_name.lock, 0, 1)
 
+#ifndef CONFIG_SPI_BLOCKING
 #define SPI_CONTEXT_INIT_SYNC(_data, _ctx_name)				\
 	._ctx_name.sync = Z_SEM_INITIALIZER(_data._ctx_name.sync, 0, 1)
+#endif /* CONFIG_SPI_BLOCKING */
 
 static inline bool spi_context_configured(struct spi_context *ctx,
 					  const struct spi_config *config)
@@ -127,10 +135,14 @@ static inline int spi_context_wait_for_completion(struct spi_context *ctx)
 		status = ctx->sync_status;
 	}
 #else
+#ifdef CONFIG_SPI_BLOCKING
+	while(!ctx->spi_complete);
+#else
 	if (k_sem_take(&ctx->sync, K_MSEC(timeout_ms))) {
 		LOG_ERR("Timeout waiting for transfer complete");
 		return -ETIMEDOUT;
 	}
+#endif /* CONFIG_SPI_BLOCKING */
 	status = ctx->sync_status;
 #endif /* CONFIG_SPI_ASYNC */
 
@@ -169,7 +181,11 @@ static inline void spi_context_complete(struct spi_context *ctx, int status)
 	}
 #else
 	ctx->sync_status = status;
+#ifdef CONFIG_SPI_BLOCKING
+	ctx->spi_complete = true;
+#else
 	k_sem_give(&ctx->sync);
+#endif /* CONFIG_SPI_BLOCKING */
 #endif /* CONFIG_SPI_ASYNC */
 }
 
@@ -269,6 +285,10 @@ void spi_context_buffers_setup(struct spi_context *ctx,
 	}
 
 	ctx->sync_status = 0;
+
+#ifdef CONFIG_SPI_BLOCKING
+	ctx->spi_complete = false;
+#endif /* CONFIG_SPI_BLOCKING */
 
 #ifdef CONFIG_SPI_SLAVE
 	ctx->recv_frames = 0;
